6c720267445f6488a67106350fc34f4cac33b4da
bharatchow <bharat.patil@doordash.com>
2024-10-11 11:28:53 +0530

    correct fields

--- ui-app/src/hooks/useGetFields.ts
+++ ui-app/src/hooks/useGetFields.ts
@@ -1,9 +1,10 @@
 import * as React from "react";
 import { getFullPathOfNode } from "../utils";
-import { OptionGroup } from "react-querybuilder";
+import { defaultOperators, OptionGroup } from "react-querybuilder";
 import { TreeNode } from "../components/TreeView/type";
 import { useDataExpressStore } from "../stores/useDataExpressStore";
 
+const commonOperators = ["null", "notNull"];
 export const useGetFields = () => {
   const leafNodes = useDataExpressStore((state) => state.values.leafNodes);
 
@@ -14,15 +15,64 @@ export const useGetFields = () => {
         label: getFullPathOfNode(v),
       };
 
-      if (v.enumValues) {
-        (returnValue as any).valueEditorType = "select";
+      const hasEnumValues =
+        Array.isArray(v.enumValues) && v.enumValues.length > 0;
+
+      if (hasEnumValues) {
+        (returnValue as any).valueEditorType = "multiselect";
 
         (returnValue as any).values = [
           {
             label: v.alias || v.name,
-            options: v.enumValues.map((v) => ({ name: v, label: v })),
+            options: (v?.enumValues || []).map((v) => ({ name: v, label: v })),
           },
         ] as OptionGroup[];
+
+        (returnValue as any).operators = [
+          ...defaultOperators.filter((op) =>
+            ["=", "!=", "in", "notIn", ...commonOperators].includes(op.name)
+          ),
+        ];
+      }
+
+      if (v.type === "date") {
+        (returnValue as any).inputType = "date";
+        (returnValue as any).operators = [
+          ...defaultOperators.filter((op) =>
+            ["=", "!=", ">", "<", ">=", "<=", ...commonOperators].includes(
+              op.name
+            )
+          ),
+        ];
+      }
+
+      if (v.type === "number" || v.type === "numeric") {
+        (returnValue as any).inputType = "number";
+        (returnValue as any).operators = [
+          ...defaultOperators.filter((op) =>
+            ["=", "!=", ">", "<", ">=", "<=", ...commonOperators].includes(
+              op.name
+            )
+          ),
+        ];
+      }
+
+      if (!hasEnumValues && (v.type === "string" || v.type === "text")) {
+        (returnValue as any).inputType = "number";
+        (returnValue as any).operators = defaultOperators.filter((op) =>
+          [
+            "contains",
+            "beginsWith",
+            "endsWith",
+            "doesNotContain",
+            "doesNotBeginWith",
+            "doesNotEndWith",
+            "null",
+            "notNull",
+            "in",
+            "notIn",
+          ].includes(op.name)
+        );
       }
 
       return returnValue;
